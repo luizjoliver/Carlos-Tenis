54d3350172482da01264b7b61ef2bac0e47f31c9
luizjoliver <luizfellipejoliver14@gmail.com>
2025-04-23 16:14:47 -0300

    add pacotes otimização - Entrega 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),ViteImageOptimizer()],
   server: {
     host: '0.0.0.0', 
     port: 5173,      
